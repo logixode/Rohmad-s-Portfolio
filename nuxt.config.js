359ddc91c744496297bd2a6654556188de4e8620
Rohmad Kurniadi <rohmad1230@gmail.com>
2021-08-31 14:55:15 +0700

    [update] adding meta for SEO purpose

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,25 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" }
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "I'am Rohmad Kurniadi a.k.a logiXode, Experienced Frontend Developer with demonstrated history of working as a Freelancer for almost 2 years. Skilled in Javascript like Vue.js, Nuxt.js, Node.js, also Tailwind CSS for Website Development and Flutter for Mobile App Development."
+      },
+      { name: "format-detection", content: "telephone=no" },
+      {
+        name: "og:title",
+        property: "og:title",
+        content: "Rohmad Kurniadi a.k.a logiXode"
+      },
+      {
+        name: "og:description",
+        property: "og:description",
+        content:
+          "Saya Rohmad Kurniadi a.k.a logiXode, seorang Frontend Developer sekaligus Mahasiswa aktif di Politeknik Negeri Banyuwangi. Berpengalaman bekerja sebagai Freelance Frontend Developer menggunakan Framework Vue.js, Nuxt.js, Node.js, dan didukung dengan Framework lain seperti Tailwind CSS, Bootstrap, dll. dan juga menggunakan Flutter dalam pengembangan Aplikasi Mobile. Berkomitmen untuk menjadi Frontend Developer yang baik dan tertarik dengan berbagai hal yang berkaitan dengan visual seperti design, photography, dan termasuk juga warna-warni syntax highlight dari text editor :D"
+      },
+      { name:"robots", content: "index, follow" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
